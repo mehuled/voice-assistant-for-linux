679faaa4d014614d34c2ed1c0c0c436b2ccb85de
Mehul Sharma <mehulcse0209@gmail.com>
2018-03-01 02:17:30 +0530

    Script to create directories based on extension! #Success

---
+++ unclutter.py
@@ -0,0 +1,29 @@
+import os
+
+directory = "%s%s" % (os.environ["HOME"],"/Desktop")
+
+extensionDirectoryPath = "%s%s" %(os.environ["HOME"],"/Desktop/")
+
+
+files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory,f))]
+
+extensionList = [] 
+
+
+for f in files :
+	
+	if "." in f :
+		
+		extension = f[::-1].split(".")[0][::-1]
+
+		if extension not in extensionList :
+			
+			extensionList.append(f[::-1].split(".")[0][::-1])
+			
+			newExtensionDirectory = "%s%s" %(extensionDirectoryPath,"%s%s" % (extension.upper(),"s"))
+			
+			os.makedirs(newExtensionDirectory)
+
+			print "Success!"
+
+	#print f[::-1].split(".")[0][::-1]
