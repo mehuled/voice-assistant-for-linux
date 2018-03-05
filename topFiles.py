a5cf0bb034fa695dd3a9c126d760163ef26816d6
Mehul Sharma <mehulcse0209@gmail.com>
2018-03-05 22:31:53 +0530

    Updated topFiles.py

--- topFiles.py
+++ topFiles.py
@@ -91,7 +91,7 @@ else :
 
 directory = os.environ["HOME"]
 
-directoriesToBeChecked = ['/Desktop','/Documents','/Downloads','/Music','/Pictures','Movies','Videos']
+directoriesToBeChecked = ['/Desktop','/Documents','/Downloads','/Music','/Pictures','/Movies','/Videos']
 
 fileinfo = []
 
